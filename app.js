31019fb68916442e538c6b5d383bf9e93840bb05
Naa003 <Nathan@scicrunch.com>
2023-04-17 20:01:42 -0700

    created post endpoint for products

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 const db = require('./db/index.js')
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}))
+
 
 app.get('/', (request, response, next) => {
     response.send('Hello World');
 })
 
+
+// Product endpoints
+
 app.get('/products', db.getProducts)
+
+app.get('/products/:id', db.getProductID)
+
+app.post('/products', db.createProduct)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`)
--- db/index.js
+++ db/index.js
@@ -17,6 +17,30 @@ const getProducts = (request, response) => {
     })
 }
 
+const getProductID = (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query('SELECT * FROM products WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+    
+}
+
+const createProduct = (request, response) => {
+    const {product_price, product_quantity, product_name, product_category} = request.body
+
+    pool.query('INSERT INTO products (product_price, product_quantity, product_name, product_category) VALUES ($1, $2, $3, $4) RETURNING *', [product_price, product_quantity, product_name, product_category], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`Product added with Product ID: ${results.rows[0].id}`)
+    })
+}
+
+
+
 // pool.query('SELECT * FROM products WHERE id = $1', [1], (err, res) => {
 //     if (err) {
 //         throw err
@@ -28,5 +52,7 @@ module.exports = {
     query: (text, params, callback) => {
       return pool.query(text, params, callback)
     },
-    getProducts
+    getProducts,
+    getProductID,
+    createProduct
   }
